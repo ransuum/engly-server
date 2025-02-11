9577e8323f28dc432ec7344841b8e8ac284c7649
ransuum <dmitrenkovalera9@gmail.com>
2025-02-11 21:04:29 +0200

    Initial commit

---
+++ src/main/java/com/engly/engly_server/EnglyServerApplication.java
@@ -0,0 +1,13 @@
+package com.engly.engly_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engly/engly_server/EnglyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engly.engly_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
