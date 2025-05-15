7ec68095e101ac34560fce7147fc1ef84d99b183
ransuum <dmitrenkovalera9@gmail.com>
2025-05-16 02:06:32 +0300

    feat: new record CookieUtils

--- src/main/java/com/engly/engly_server/security/cookiemanagement/CookieUtils.java
+++ src/main/java/com/engly/engly_server/security/cookiemanagement/CookieUtils.java
@@ -2,22 +2,40 @@ package com.engly.engly_server.security.cookiemanagement;
 
 import com.engly.engly_server.exception.TokenNotFoundException;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.Arrays;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class CookieUtils {
+public record CookieUtils(Cookie[] cookies) {
+    public CookieUtils(Cookie[] cookies) {
+        this.cookies = cookies == null ? null : cookies.clone();
+    }
 
-    public static String getRefreshTokenCookie(HttpServletRequest request) {
-        final Cookie[] cookies = request.getCookies();
+    public String getRefreshTokenCookie() {
         if (cookies == null) return null;
         return Arrays.stream(cookies)
                 .filter(cookie -> "refreshToken".equals(cookie.getName()))
                 .findFirst()
                 .map(Cookie::getValue)
                 .orElseThrow(() -> new TokenNotFoundException("Refresh token is not found in cookies"));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CookieUtils that = (CookieUtils) o;
+        return Arrays.equals(cookies(), that.cookies());
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Arrays.hashCode(cookies);
+    }
+
+    @Override
+    public @NonNull String toString() {
+        return "CookieUtils{" +
+                "cookies=" + Arrays.toString(cookies) +
+                '}';
     }
 }
