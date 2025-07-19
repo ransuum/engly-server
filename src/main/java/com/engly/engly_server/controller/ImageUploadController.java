fef06ddbd641f255deeadb5c37fd60fc2d60d949
olexandr2009 <hristevich.ua@gmail.com>
2025-07-19 12:09:37 +0300

    removed unused imports

--- src/main/java/com/engly/engly_server/controller/ImageUploadController.java
+++ src/main/java/com/engly/engly_server/controller/ImageUploadController.java
@@ -3,9 +3,7 @@ package com.engly.engly_server.controller;
 import com.engly.engly_server.googledrive.FileUploadResponse;
 import com.engly.engly_server.googledrive.GoogleDriveService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -13,9 +11,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
