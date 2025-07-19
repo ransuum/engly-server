592a1d76a5454c86c0b894ef97f818f1dd3348cc
olexandr2009 <hristevich.ua@gmail.com>
2025-07-19 20:33:46 +0300

    changed exception

--- src/main/java/com/engly/engly_server/googleDrive/GoogleDriveConfig.java
+++ src/main/java/com/engly/engly_server/googleDrive/GoogleDriveConfig.java
@@ -1,6 +1,5 @@
 package com.engly.engly_server.googleDrive;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
@@ -10,15 +9,10 @@ import com.google.auth.oauth2.UserCredentials;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-
-import java.io.InputStreamReader;
 
 @Configuration
 public class GoogleDriveConfig {
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    @Value("classpath:googleDriveCreds.json")
-    Resource resourceFile;
     @Value("${google.drive.refreshToken}")
     String refreshToken;
     @Value("${google.drive.clientSecret}")
@@ -31,15 +25,10 @@ public class GoogleDriveConfig {
      */
     @Bean
     public UserCredentials userCredentials() throws Exception {
-        // Load client secrets.
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-                JSON_FACTORY, new InputStreamReader(resourceFile.getInputStream()));
-
-
         // Build credentials directly with refresh token
         UserCredentials credentials = UserCredentials.newBuilder()
-                .setClientId(clientSecrets.getWeb().getClientId())
-                .setClientSecret(clientSecrets.getWeb().getClientSecret())
+                .setClientId(clientId)
+                .setClientSecret(clientSecret)
                 .setRefreshToken(refreshToken)
                 .build();
 
