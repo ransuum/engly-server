79800643c3ce5d8cb1199b6e3bfd23e37b5fbfc1
valerii_dmytrenko <dmitrenkovalera9@gmail.com>
2025-08-06 22:07:13 +0300

    fix: mail sender

--- src/main/java/com/engly/engly_server/service/common/impl/EmailServiceImpl.java
+++ src/main/java/com/engly/engly_server/service/common/impl/EmailServiceImpl.java
@@ -1,33 +1,34 @@
 package com.engly.engly_server.service.common.impl;
 
 import com.engly.engly_server.service.common.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.CompletableFuture;
-
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl implements EmailService {
     private final JavaMailSender mailSender;
 
     @Value("${app.email.notification.subject}")
     private String subject;
 
-    public EmailServiceImpl(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
-    @Async
+    @Async("virtualThreadExecutor")
     public void sendEmail(String to, String body) {
-        CompletableFuture.runAsync(() -> {
+        try {
             var message = new SimpleMailMessage();
             message.setTo(to);
             message.setSubject(subject);
             message.setText(body);
             mailSender.send(message);
-        });
+            log.info("Email sent successfully to: {}", to);
+        } catch (Exception e) {
+            log.error("Failed to send email to: {}", to, e);
+        }
     }
 }
