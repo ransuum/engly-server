cc77a722a5f8ddf1ed911e172f7549687ca304bc
valerii_dmytrenko <dmitrenkovalera9@gmail.com>
2025-08-08 19:10:22 +0300

    fix: NULLABLE of image

--- src/main/resources/db/migration/V1__ENTITIES.sql
+++ src/main/resources/db/migration/V1__ENTITIES.sql
@@ -118,7 +118,7 @@ CREATE TABLE users
     id             VARCHAR(255)                NOT NULL,
     user_name      VARCHAR(255)                NOT NULL,
     email_id       VARCHAR(255)                NOT NULL,
-    img_url        VARCHAR(255)                NOT NULL,
+    img_url        VARCHAR(255),
     password       VARCHAR(255)                NOT NULL,
     roles          VARCHAR(255)                NOT NULL,
     email_verified BOOLEAN                     NOT NULL,
