b2d50749997179e85e5453b3ee02a03821679707
Valerii Dmytrenko <valerii_dmytrenko@epam.com>
2025-09-18 00:19:17 +0300

    fix: cache

--- src/main/java/com/engly/engly_server/mapper/MessageMapper.java
+++ src/main/java/com/engly/engly_server/mapper/MessageMapper.java
@@ -1,15 +1,16 @@
 package com.engly.engly_server.mapper;
 
 import com.engly.engly_server.models.dto.response.MessagesDto;
+import com.engly.engly_server.models.dto.response.RoomDtoShort;
 import com.engly.engly_server.models.entity.Message;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = {UserMapper.class, CategoryMapper.class})
+@Mapper(uses = {UserMapper.class, CategoryMapper.class},
+        componentModel = "spring")
 public interface MessageMapper {
-    MessageMapper INSTANCE = Mappers.getMapper(MessageMapper.class);
 
-    @Mapping(source = "message.roomId", target = "roomId")
-    MessagesDto toMessageDto(Message message);
+    @Mapping(target = "id", source = "message.id")
+    MessagesDto toMessageDto(Message message, RoomDtoShort room);
+
 }
--- src/main/java/com/engly/engly_server/utils/cache/CacheName.java
+++ src/main/java/com/engly/engly_server/utils/cache/CacheName.java
@@ -21,6 +21,7 @@ public class CacheName {
     public static final String ROOMS_BY_CATEGORY = "rooms-by-category";
     public static final String ROOMS_BY_CRITERIA = "rooms-by-criteria";
     public static final String ROOM_ENTITY_ID = "room-entity-by-id";
+    public static final String ROOM_SHORT_ID = "room-short-by-id";
 
     public static final String CATEGORY_ENTITY_ID = "category-entity-by-id";
     public static final String CATEGORY_NAME = "category-by-name";
@@ -43,7 +44,7 @@ public class CacheName {
     public static final List<String> CACHES = Arrays.asList(
             USER_ID, ALL_USER, USER_BY_EMAIL, USER_PROFILES, USER_ID_BY_EMAIL, USERNAME_BY_EMAIL,ROOM_DTO_ID,
             USER_SETTINGS, ROOMS_BY_CATEGORY, ROOM_ENTITY_ID, ROOMS_BY_CRITERIA, COUNT_PARTICIPANTS,
-            CATEGORY_ENTITY_ID, CATEGORY_NAME, ALL_CATEGORIES, MESSAGE_ID, CATEGORY_ID_BY_NAME,
+            CATEGORY_ENTITY_ID, CATEGORY_NAME, ALL_CATEGORIES, MESSAGE_ID, CATEGORY_ID_BY_NAME, ROOM_SHORT_ID,
             MESSAGES_BY_ROOM_NATIVE, MESSAGE_COUNT_BY_ROOM, MESSAGES_BY_CRITERIA, PARTICIPANTS_BY_ROOM,
             PARTICIPANT_EXISTS, MESSAGE_READ_STATUS, USERS_WHO_READ_MESSAGE, USER_EXISTS_BY_ID, USER_ENTITY_ID
     );
