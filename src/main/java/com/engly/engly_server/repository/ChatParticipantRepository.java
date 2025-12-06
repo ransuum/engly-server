1d430078b94e29014a4485810f2df10a9b06a476
Valerii Dmytrenko <valerii_dmytrenko@epam.com>
2025-12-06 21:22:30 +0200

    feat: jspecify annotation

--- src/main/java/com/engly/engly_server/repository/ChatParticipantRepository.java
+++ src/main/java/com/engly/engly_server/repository/ChatParticipantRepository.java
@@ -1,8 +1,6 @@
 package com.engly.engly_server.repository;
 
 import com.engly.engly_server.models.entity.ChatParticipants;
-import lombok.NonNull;
-import org.jspecify.annotations.NullMarked;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,10 +10,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-@NullMarked
 public interface ChatParticipantRepository extends JpaRepository<ChatParticipants, String> {
 
-    Page<@NonNull ChatParticipants> findAllByRoomId(@Param("roomId") String roomId, Pageable pageable);
+    Page<ChatParticipants> findAllByRoomId(@Param("roomId") String roomId, Pageable pageable);
 
     boolean existsByRoomIdAndUserId(String roomId, String userId);
 
--- src/main/java/com/engly/engly_server/repository/NotificationRepository.java
+++ src/main/java/com/engly/engly_server/repository/NotificationRepository.java
@@ -1,9 +1,7 @@
 package com.engly.engly_server.repository;
 
 import com.engly.engly_server.models.entity.Notifications;
-import org.jspecify.annotations.NullMarked;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@NullMarked
 public interface NotificationRepository extends JpaRepository<Notifications, String> {
 }
